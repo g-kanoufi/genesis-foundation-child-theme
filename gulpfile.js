6586e7b572c939838b3a3d946dc44b59e4a1bf39
Guillaume Kanoufi <g.kanoufi@gmail.com>
2015-09-10 13:10:05 +0100

    Gulpfile ready?

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,273 @@
-var gulp      = require( 'gulp' ),
-  minifycss   = require( 'gulp-minify-css' ),
-  watch       = require( 'gulp-watch' ),
-  uglify      = require( 'gulp-uglify' ),
-  sass        = require( 'gulp-sass' ),
-  plumber     = require( 'gulp-plumber' ),
-  browserSync = require( 'browser-sync' ),
-  jshint      = require( 'gulp-jshint' ),
-  stylish     = require( 'jshint-stylish' ),
-  reload      = browserSync.reload;
+/**
+ *
+ * Gulpfile setup adapted from advanced-gulp-wordpress
+ *
+ * @since 1.0.0
+ * @authors Ahmad Awais, @digisavvy, @desaiuditd, @jb510, @dmassiani and @Maxlopez
+ * @package genesis-foundation-child-theme
+ */
 
 
-// We need to set up an error handler (which gulp-plumber calls).
-// Otherwise, Gulp will exit if an error occurs, which is what we don't want.
-var onError = function( err ) {
-  console.log( 'An error occured:', err );
-  this.emit( 'end' );
-}
+// Project configuration
+var project             = 'genesis-foundation-child-theme', // Project name, used for build zip.
+    url                 = 'foundation.dev', // Local Development URL for BrowserSync. Change as-needed.
+    bower               = './bower_components/'; // Not truly using this yet, more or less playing right now. TO-DO Place in Dev branch
+    build               = './buildtheme/', // Files that you want to package into a zip go here
+    buildInclude        = [
+                // include common file types
+                '**/*.php',
+                '**/*.html',
+                '**/*.css',
+                '**/*.js',
+                '**/*.svg',
+                //No need as woff is well supported '**/*.ttf',
+                '**/*.woff',
+                '**/*.woff2',
+
+                // include specific files and folders
+                'screenshot.png',
+
+                // exclude files and folders
+                '!node_modules/**/*',
+                '!bower_components/**/*',
+                '!style.css.map',
+                '!styles/*'
+
+            ];
+
+// Load plugins
+    var gulp     = require('gulp'),
+        browserSync  = require('browser-sync'), // Asynchronous browser loading on .scss file changes
+        reload       = browserSync.reload,
+        autoprefixer = require('gulp-autoprefixer'), // Autoprefixing magic
+        minifycss    = require('gulp-uglifycss'),
+        filter       = require('gulp-filter'),
+        uglify       = require('gulp-uglify'),
+        imagemin     = require('gulp-imagemin'),
+        newer        = require('gulp-newer'),
+        rename       = require('gulp-rename'),
+        concat       = require('gulp-concat'),
+        notify       = require('gulp-notify'),
+        cmq          = require('gulp-combine-media-queries'),
+        runSequence  = require('gulp-run-sequence'),
+        sass         = require('gulp-sass'),
+        plugins      = require('gulp-load-plugins')({ camelize: true }),
+        ignore       = require('gulp-ignore'), // Helps with ignoring files and directories in our run tasks
+        rimraf       = require('gulp-rimraf'), // Helps with removing files and directories in our run tasks
+        zip          = require('gulp-zip'), // Using to zip up our packaged theme into a tasty zip file that can be installed in WordPress!
+        plumber      = require('gulp-plumber'), // Helps prevent stream crashing on errors
+        cache        = require('gulp-cache'),
+        sourcemaps   = require('gulp-sourcemaps');
 
 
-// Our development server that serves all the assets and reloads the browser
-// when any of them change (hence the watch calls in it)
-gulp.task( 'server', function() {
-  browserSync.init({
-    // change 'localhost' to whatever your local Nginx/Apache vhost is set
-    // most commonly 'http://localhost/' or 'http://127.0.0.1/'
-    // See http://www.browsersync.io/docs/options/ for more information
-    proxy: 'foundation.dev'
-  });
+/**
+ * Browser Sync
+ *
+ * Asynchronous browser syncing of assets across multiple devices!! Watches for changes to js, image and php files
+ * Although, I think this is redundant, since we have a watch task that does this already.
+*/
+gulp.task('browser-sync', function() {
+    var files = [
+                    '**/*.php',
+                    '**/*.{png,jpg,gif}'
+                ];
+    browserSync.init(files, {
 
-  // Reload the browser if any .php file changes within this directory
-  watch( './**/*.php', reload);
+        // Read here http://www.browsersync.io/docs/options/
+        proxy: url,
 
-  // Recompile sass into CSS whenever we update any of the source files
-  watch( './styles/**/*.scss', function() {
-    gulp.start( 'scss' );
-  });
+        // port: 8080,
 
-  // Watch our JavaScript files and report any errors. May ruin your day.
-  watch( './js/**/*.js', function() {
-    gulp.start( 'jshint' );
-  })
+        // Tunnel the Browsersync server through a random Public URL
+        tunnel: true,
+
+        // Inject CSS changes
+        injectChanges: true
+
+    });
 });
 
 
-// Processes SASS and reloads browser.
-gulp.task( 'scss', function() {
-  return gulp.src( './styles/app.scss' )
-    .pipe( plumber( { errorHandler: onError } ) )
-    .pipe( sass() )
-    .pipe( gulp.dest( '.' ) )
-    .pipe( reload( { stream: true } ) );
-} );
 
+/**
+ * Styles
+ *
+ * Looking at src/sass and compiling the files into Expanded format, Autoprefixing and sending the files to the build folder
+ *
+ * Sass output styles: https://web-design-weekly.com/2014/06/15/different-sass-output-styles/
+*/
+gulp.task('styles', function () {
+        gulp.src('./styles/app.scss')
+                .pipe(plumber())
+                .pipe(sourcemaps.init())
+                .pipe(sass({
+                    errLogToConsole: true,
 
-// Jshint outputs any kind of javascript problems you might have
-// Only checks javascript files inside /js directory
-gulp.task( 'jshint', function() {
-  return gulp.src( './js/**/*.js' )
-    .pipe( jshint( '.jshintrc' ) )
-    .pipe( jshint.reporter( stylish ) )
-    .pipe( jshint.reporter( 'fail' ) );
+                    outputStyle: 'compressed',
+                    //outputStyle: 'compact',
+                    // outputStyle: 'nested',
+                    // outputStyle: 'expanded',
+                    precision: 10
+                }))
+                .pipe(sourcemaps.write({includeContent: false}))
+                .pipe(sourcemaps.init({loadMaps: true}))
+                .pipe(autoprefixer('last 2 version', '> 1%', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+                .pipe(sourcemaps.write('.'))
+                .pipe(plumber.stop())
+                .pipe(gulp.dest('./'))
+                .pipe(filter('**/*.css')) // Filtering stream to only css files
+                .pipe(cmq()) // Combines Media Queries
+                .pipe(reload({stream:true})) // Inject Styles when style file is created
+                .pipe(rename({ suffix: '.min' }))
+                .pipe(minifycss({
+                    maxLineLen: 80
+                }))
+                .pipe(gulp.dest('./'))
+                .pipe(reload({stream:true})) // Inject Styles when min style file is created
+                .pipe(notify({ message: 'Styles task complete', onLast: true }))
 });
 
 
-// The default task. When developting just run 'gulp' and this is what will be ran.
-// Note the second parameter, those are dependency tasks which need to be done
-// before the main function (third parameter) is called.
-gulp.task( 'default', [ 'scss', 'server' ], function() {
-  console.log('done');
-} );
+/**
+ * Scripts: Vendors
+ *
+ * Look at src/js and concatenate those files, send them to assets/js where we then minimize the concatenated file.
+*/
+gulp.task('vendorsJs', function() {
+    return      gulp.src(['./assets/js/vendor/*.js', bower+'**/*.js'])
+                .pipe(concat('vendors.js'))
+                .pipe(gulp.dest('./assets/js'))
+                .pipe(rename( {
+                    basename: "vendors",
+                    suffix: '.min'
+                }))
+                .pipe(uglify())
+                .pipe(gulp.dest('./assets/js/'))
+                .pipe(notify({ message: 'Vendor scripts task complete', onLast: true }));
+});
+
+
+/**
+ * Scripts: Custom
+ *
+ * Look at src/js and concatenate those files, send them to assets/js where we then minimize the concatenated file.
+*/
+
+gulp.task('scriptsJs', function() {
+    return      gulp.src('./js/scripts.js')
+                .pipe(gulp.dest('./js/min/'))
+                .pipe(rename( {
+                    basename: "scripts",
+                    suffix: '.min'
+                }))
+                .pipe(uglify())
+                .pipe(gulp.dest('./js/'))
+                .pipe(notify({ message: 'Custom scripts task complete', onLast: true }));
+});
+
+
+/**
+ * Images
+ *
+ * Look at src/images, optimize the images and send them to the appropriate place
+*/
+gulp.task('images', function() {
+
+// Add the newer pipe to pass through newer images only
+    return      gulp.src(['./images/**/*.{png,jpg,gif}'])
+                .pipe(newer('./images/'))
+                .pipe(rimraf({ force: true }))
+                .pipe(imagemin({ optimizationLevel: 7, progressive: true, interlaced: true }))
+                .pipe(gulp.dest('./images/'))
+                .pipe( notify( { message: 'Images task complete', onLast: true } ) );
+});
+
+
+/**
+ * Clean gulp cache
+ */
+ gulp.task('clear', function () {
+   cache.clearAll();
+ });
+
+
+ /**
+  * Clean tasks for zip
+  *
+  * Being a little overzealous, but we're cleaning out the build folder, codekit-cache directory and annoying DS_Store files and Also
+  * clearing out unoptimized image files in zip as those will have been moved and optimized
+ */
+
+ gulp.task('cleanup', function() {
+    return      gulp.src(['./bower_components', '**/.sass-cache','**/.DS_Store'], { read: false }) // much faster
+                .pipe(ignore('node_modules/**')) //Example of a directory to ignore
+                .pipe(rimraf({ force: true }))
+                // .pipe(notify({ message: 'Clean task complete', onLast: true }));
+ });
+ gulp.task('cleanupFinal', function() {
+    return      gulp.src(['./bower_components','**/.sass-cache','**/.DS_Store'], { read: false }) // much faster
+                .pipe(ignore('node_modules/**')) //Example of a directory to ignore
+                .pipe(rimraf({ force: true }))
+                // .pipe(notify({ message: 'Clean task complete', onLast: true }));
+ });
+
+ /**
+  * Build task that moves essential theme files for production-ready sites
+  *
+  * buildFiles copies all the files in buildInclude to build folder - check variable values at the top
+  * buildImages copies all the images from img folder in assets while ignoring images inside raw folder if any
+  */
+
+  gulp.task('buildFiles', function() {
+    return      gulp.src(buildInclude)
+                .pipe(gulp.dest(build))
+                .pipe(notify({ message: 'Copy from buildFiles complete', onLast: true }));
+  });
+
+
+/**
+* Images
+*
+* Look at src/images, optimize the images and send them to the appropriate place
+*/
+gulp.task('buildImages', function() {
+    return      gulp.src(['/images/**/*'])
+                .pipe(gulp.dest(build+'/images/'))
+                .pipe(plugins.notify({ message: 'Images copied to buildTheme folder', onLast: true }));
+});
+
+ /**
+  * Zipping build directory for distribution
+  *
+  * Taking the build folder, which has been cleaned, containing optimized files and zipping it up to send out as an installable theme
+ */
+ gulp.task('buildZip', function () {
+    // return   gulp.src([build+'/**/', './.jshintrc','./.bowerrc','./.gitignore' ])
+    return      gulp.src(build+'/**/')
+                .pipe(zip(project+'.zip'))
+                .pipe(gulp.dest('./'))
+                .pipe(notify({ message: 'Zip task complete', onLast: true }));
+ });
+
+
+ // ==== TASKS ==== //
+ /**
+  * Gulp Default Task
+  *
+  * Compiles styles, fires-up browser sync, watches js and php files. Note browser sync task watches php files
+  *
+ */
+
+ // Package Distributable Theme
+ gulp.task('build', function(cb) {
+    runSequence('styles', 'cleanup', 'vendorsJs', 'scriptsJs',  'buildFiles', 'buildImages', 'buildZip','cleanupFinal', cb);
+ });
+
+
+ // Watch Task
+ gulp.task('default', ['styles', 'vendorsJs', 'scriptsJs', 'images', 'browser-sync'], function () {
+    gulp.watch('./images/**/*', ['images']);
+    gulp.watch('./styles/*.scss', ['styles']);
+    gulp.watch('./js/*.js', ['scriptsJs', browserSync.reload]);
+
+ });
