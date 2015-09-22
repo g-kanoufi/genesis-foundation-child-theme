acb34e534a34779a5ed17a43c18bce92edaee3e2
Guillaume Kanoufi <g.kanoufi@gmail.com>
2015-09-22 12:59:45 +0100

    Updated app.js file with @codekit prepend for foundation scripts

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+// @codekit-prepend '../bower_components/foundation/js/vendor/jquery.cookie.js';
+// @codekit-prepend '../bower_components/foundation/js/vendor/fastclick.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.abide.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.accordion.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.alert.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.clearing.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.dropdown.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.equalizer.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.interchange.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.joyride.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.magellan.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.offcanvas.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.orbit.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.reveal.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.slider.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.tab.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.tooltip.js';
+// @codekit-prepend '../bower_components/foundation/js/foundation/foundation.topbar.js';
+
+
+
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 (function($){
